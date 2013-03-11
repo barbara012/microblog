807e0058703b50974fb1595ae2d3493a004671e6
GilbertSun_ubuntu <szb4321@gmail.com>
2013-03-12 06:54:15 +0800

    添加页面控制权限

--- routes/index.js
+++ routes/index.js
@@ -10,10 +10,12 @@ module.exports = function(app){
     res.render('index', { title: '首页'});
   });
 
+  app.get('/reg', chkNotLogin);
   app.get('/reg', function(req, res){
     res.render('reg', { title: '注册'});
   });
 
+  app.post('/reg', chkNotLogin);
   app.post('/reg', function(req, res){
     //检验两次输入的结果是否一致
     if(req.body["password-repeat"] != req.body["password"]){
@@ -50,10 +52,12 @@ module.exports = function(app){
     });
   });
 
+  app.get('/login', chkNotLogin);
   app.get('/login', function(req, res){
     res.render('login', {title:'用户登入'});
   });
 
+  app.post('/login', chkNotLogin);
   app.post('/login', function(req, res){
     //生成口令的散列值
     var md5 = crypto.createHash('md5');
@@ -74,9 +78,26 @@ module.exports = function(app){
     });    
   });
 
+  app.get('/logout', chkLogin);
   app.get('/logout', function(req, res){
     req.session.user = null;
     req.flash('success', '登出成功');
     res.redirect('/');    
   });
+}
+
+function chkLogin(req, res, next){
+  if(!req.session.user){
+    req.flash('error', '未登入');
+    return res.redirect('/login');
+  }
+  next();
+}
+
+function chkNotLogin(req, res, next){
+  if(req.session.user){
+    req.flash('error', '已登入');
+    return res.redirect('/');
+  }
+  next();
 }
