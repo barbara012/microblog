361346e970d7572e8963ab7b3ce9f8279b0ea143
GilbertSun_ubuntu <szb4321@gmail.com>
2013-03-14 03:22:06 +0800

    首页显示所有的微博

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,15 @@ var Post = require('../models/post.js');
 
 module.exports = function(app){
   app.get('/', function(req, res){
-    res.render('index', { title: '首页'});
+    Post.get(null, function(err, posts){
+      if(err){
+        posts = [];
+      }
+      res.render('index', { 
+        title: '首页',
+        posts: posts
+      });
+    });
   });
 
   app.get('/reg', chkNotLogin);
